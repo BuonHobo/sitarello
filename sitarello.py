704ba90e8cbf83b1554007e8095a30a0d59fd085
BuonHobo <naturytropik@gmail.com>
2021-02-18 19:13:23 +0100

    aggiunto crawler e modificato template

--- sitarello.py
+++ sitarello.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template  # pip install flask
+from flask import Flask, render_template  # pip install flask
+import crawler
 app = Flask(__name__)
-
+articoli=crawler.get_articoli()
 
 @app.route('/')
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",articoli=articoli)
 
 
 @app.route('/about')
